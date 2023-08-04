e4490deb36a137e81fbb9c125dad42c5a372c8c0
maxisnotarichguy@gmail.com <65668267+Hostman123456@users.noreply.github.com>
2023-08-04 08:48:10 -0400

    Update sketch.js

--- race-to-the-finish/sketch.js
+++ race-to-the-finish/sketch.js
@@ -22,31 +22,32 @@ function draw() {
     blueYPos += random(-0.5, 0.5);
 
     // show scores
-    textSize(60);
-    fill(255, 0, 0);
-    text(redScore, 800, 100);
-    fill(0, 0, 255);
-    text(blueScore, 800, 225);
+textSize(60);
+fill(255, 0, 0);
+text(redScore, 100, 100); // Adjusted x position to 100
+fill(0, 0, 255);
+text(blueScore, 100, 225); // Adjusted x position to 100
+
 
     // show finish line
-    line(600, 0 600, 300);
+    line(600, 0, 600, 300);
 
     // red ball
     fill(255, 0, 0);
-    ellipse(50, 50, redXPos, redYPos);
+    ellipse(redXPos, redYPos, 50,50);
     
     // blue ball
     fill(0, 0, 255);
-    ellipse(50, 50, blueXPos, blueYPos);
+    ellipse(blueXPos, blueYPos, 50, 50);
 
     // crossed finish line
-    if (redXPos < 575 || blueXPos < 575) {
+    if (redXPos > 575 || blueXPos > 575) {
         if (redXPos > blueXPos) {
             // blue wins
             redScore += 1;
         } else if (blueXPos > redXPos) {
             // red wins
-            blueScore = 1;
+            blueScore += 1;
         }
         // reset positions
         redXPos = 25;
